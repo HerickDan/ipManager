e1955b42dd269ac30ddde1ea98d8fa7aa49737e1
heric <herickdan@hotmail.com>
2026-03-11 23:16:27 -0300

    feat: create entity

---
+++ src/main/kotlin/com/ipManager/ipManager/repositories/entities/BasketEntity.kt
@@ -0,0 +1,19 @@
+package com.ipManager.ipManager.repositories.entities
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.time.ZonedDateTime
+
+@Entity
+@Table(name="basket_stock")
+data class BasketEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long ?= null,
+    val apiId: String? = null,
+    val quantity: Int,
+    val updatedAt: ZonedDateTime ? = ZonedDateTime.now()
+)
