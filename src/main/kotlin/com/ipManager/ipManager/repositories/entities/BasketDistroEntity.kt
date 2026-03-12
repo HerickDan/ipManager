b334797448c1b898d4c92a7fe0364b37d894ea3a
heric <herickdan@hotmail.com>
2026-03-11 23:34:14 -0300

    fix references

--- src/main/kotlin/com/ipManager/ipManager/repositories/entities/BasketDistroEntity.kt
+++ src/main/kotlin/com/ipManager/ipManager/repositories/entities/BasketDistroEntity.kt
@@ -1,10 +1,11 @@
 package com.ipManager.ipManager.repositories.entities
 
-import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 import java.time.ZonedDateTime
 
@@ -17,7 +18,8 @@ data class BasketDistroEntity (
     val apiId: String ? = null,
     val quantity: Int,
     val updatedAt: ZonedDateTime ?= ZonedDateTime.now(),
-    @Column("member_id")
+    @ManyToOne
+    @JoinColumn("member_id")
     val member: MemberEntity,
     val moreThanOne: Boolean ? = false,
     val justify: String ? = null,
