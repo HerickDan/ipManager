f716a45f0b674a4e73ef2757b8df5f4411e92e4c
heric <herickdan@hotmail.com>
2026-03-14 17:52:38 -0300

    fix: add register about the distro

--- src/main/kotlin/com/ipManager/ipManager/repositories/interfaces/BasketDistroRepository.kt
+++ src/main/kotlin/com/ipManager/ipManager/repositories/interfaces/BasketDistroRepository.kt
@@ -1,9 +1,10 @@
 package com.ipManager.ipManager.repositories.interfaces
 
+import com.ipManager.ipManager.repositories.entities.BasketDistroEntity
 import com.ipManager.ipManager.repositories.entities.BasketEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface BasketDistroRepository: JpaRepository<Long, BasketEntity> {
+interface BasketDistroRepository: JpaRepository<BasketDistroEntity,Long> {
 }
