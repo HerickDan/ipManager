585647ebd66a890216beaee9ff5c5fe17383efc5
HerickDan <herickdan@hotmail.com>
2026-06-08 19:32:16 -0300

    Rename task

--- src/main/kotlin/com/ipManager/ipManager/api/requests/CreateAdminRequest.kt
+++ src/main/kotlin/com/ipManager/ipManager/api/requests/CreateAdminRequest.kt
@@ -1,16 +1,16 @@
 package com.ipManager.ipManager.api.requests
 
-import com.ipManager.ipManager.api.dto.MemberDto
+import com.ipManager.ipManager.api.dto.AdminDto
 
-data class CreateMemberRequest(
+data class CreateAdminRequest(
     val firstName: String,
     val lastName: String,
     val password: String,
     val email: String,
     val description: String ? = "Default description"
     ) {
-    fun toDto(): MemberDto =
-        MemberDto(
+    fun toDto(): AdminDto =
+        AdminDto(
             firstName = this.email,
             lastName = this.lastName,
             password = this.password,
