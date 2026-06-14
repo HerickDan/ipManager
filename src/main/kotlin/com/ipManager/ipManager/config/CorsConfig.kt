c42b5caff59c7b19b2febd2460013bf187d796a6
HerickDan <herickdan@hotmail.com>
2026-06-13 23:53:54 -0300

    final updates

--- src/main/kotlin/com/ipManager/ipManager/config/CorsConfig.kt
+++ src/main/kotlin/com/ipManager/ipManager/config/CorsConfig.kt
@@ -1,5 +1,6 @@
 package com.ipManager.ipManager.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.cors.CorsConfiguration
@@ -8,15 +9,20 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.filter.CorsFilter
 
 @Configuration
-class CorsConfig {
+class CorsConfig(
+    @Value("\${cors.allowed-origins}")
+    private val client: String
+) {
 
     @Bean
-    fun corsConfigurationSource(): CorsConfigurationSource {
+    fun corsConfigurationSource(
+
+    ): CorsConfigurationSource {
         val configuration = CorsConfiguration()
 
         configuration.allowedOrigins = listOf(
             "http://localhost:3000",
-            "https://seu-frontend.vercel.app"
+            client
         )
 
         configuration.allowedMethods = listOf("*")
